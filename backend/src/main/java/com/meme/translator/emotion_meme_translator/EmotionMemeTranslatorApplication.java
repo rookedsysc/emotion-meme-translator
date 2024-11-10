907bd434024bd1a02dd977c26e5f6a69f372455a
rookedsysc <rookedsysc36@gmail.com>
2024-11-10 17:53:34 +0900

    backend project init

---
+++ backend/src/main/java/com/meme/translator/emotion_meme_translator/EmotionMemeTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.meme.translator.emotion_meme_translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionMemeTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionMemeTranslatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/meme/translator/emotion_meme_translator/EmotionMemeTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meme.translator.emotion_meme_translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionMemeTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
