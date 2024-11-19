0ea030d82c9472b27d8df37bdfdac23c94e26f15
SeungjaeDanLee <vqs210@gmail.com>
2024-11-19 21:14:27 +0900

    update: 이미지 경로 변경

--- backend/src/main/java/com/meme/translator/emotion_meme_translator/data/EmotionMemeData.java
+++ backend/src/main/java/com/meme/translator/emotion_meme_translator/data/EmotionMemeData.java
@@ -10,7 +10,7 @@ public class EmotionMemeData {
             new EmotionMemeDto(1, "안녕히 계세요 여러분~", "/images/sample2.jpeg", "안녕히 계세요 여러분~ 전 이 세상의 모든 굴레와 속박을 벗어던지고 제 행복을 찾아 떠납니다~"),
             new EmotionMemeDto(2, "난 가끔 눈물을 흘린다.", "/images/sample3.png", "난… ㄱㅏ끔…\\n눈물을 흘린ㄷㅏ…\\nㄱㅏ끔은 눈물을 참을 수 없는 ㄴㅐ가 별루ㄷㅏ…\\n맘이 ㅇㅏㅍㅏㅅㅓ…\\n소ㄹㅣ 치며… 울 수 있ㄷㅏ는건…\\n좋은ㄱㅓㅇㅑ…\\nㅁㅓ… 꼭 슬ㅍㅓㅇㅑ만우는건ㅇㅏ니잖아…^^\\n난...눈물ㅇㅣ…좋다…\\nㅇㅏ니…\\nㅁㅓ리가ㅇㅏ닌…\\n맘으로…우는ㄴㅐㄱㅏ좋다…"),
             new EmotionMemeDto(3, "사과해요. 나한테!", "/images/sample4.jpg", "내가 그쪽을 좋아한 걸 왜 사과해야 되요? 남자가 여자 좋아한게 무슨 문제라고.\\n만약 사과를 할려면 그쪽이 해야지.\\n나는 그쪽이 좋은데 그쪽은 내가 싫으니까,\\n싫어서 미안하다고 그쪽이 사과해야 되는 거아니예요?\\n사과해요, 나한테! "),
-            new EmotionMemeDto(4, "원영적 사고", "/images/sample5.jpg", "원영적 사고"),
+            new EmotionMemeDto(4, "원영적 사고", "/images/sample5.png", "원영적 사고"),
             new EmotionMemeDto(5, "빠스", "/images/sample6.jpg", "빠스잖아")
     );
 }
