1672d7bfe7918961c42e851bf6cd291a942dc785
jjeongmin98 <eodjr05@naver.com>
2024-12-13 18:28:52 +0900

    test

--- jenkins_start.sh
+++
@@ -1,6 +0,0 @@
-docker run -d --name jenkins -p 2024:2024 \
-  -v $(pwd)/jenkins:/var/jenkins_home \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $(pwd):/var \
-  --restart unless-stopped \
-  jenkins
